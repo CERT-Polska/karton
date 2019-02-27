cf40b2f655f5392d875e38ae40c899b6ec3f877a
Hubert Barc <des@cert.pl>
2019-02-27 11:59:41 +0100

    ver change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="karton",
-      version="1.1.0",
+      version="1.1.1",
       description="Base library for karton subsystems",
       package_dir={'karton': 'karton'},
       packages=['karton'],
