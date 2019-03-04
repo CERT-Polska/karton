df0141cc92d3f3be19a18a7e666a69583afa1843
Hubert Barc <des@cert.pl>
2019-03-04 12:17:38 +0100

    export AsyncTask

--- karton/__init__.py
+++ karton/__init__.py
@@ -1,4 +1,4 @@
 from .karton import Karton, Producer, Consumer
-from .task import Task
+from .task import Task, AsyncTask
 from .resource import Resource, DirectoryResource
 from .config import Config
