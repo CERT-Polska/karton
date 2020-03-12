ccb4b7d75839c034e1da69ff1eaa16631579c185
Pawel Srokosz <pawel.srokosz@cert.pl>
2020-03-12 13:42:29 +0100

    Added SEARCH_PATH to karton2.Config

--- karton/config.py
+++ karton/config.py
@@ -1,3 +1,5 @@
+import os
+
 try:
     import configparser
 except ImportError:
@@ -7,14 +9,31 @@ except ImportError:
 
 
 class Config(object):
-    def __init__(self, path):
-        """
-        Simple config loader, minio and RabbitMQ sections are required, for more information look at config.ini.example
+    SEARCH_PATHS = [
+        "/etc/karton/karton.ini",
+        os.path.expanduser("~/.config/karton/karton.ini"),
+        "./karton.ini",
+    ]
 
-        :param path: path to config.ini
+    def __init__(self, path=None):
         """
+        Simple config loader. Minio and redis sections are required, for more information look at config.ini.example
+
+        Loads configuration from paths specified below (in provided order):
+        - /etc/karton/karton.ini (global)
+        - ~/.config/karton/karton.ini (user local)
+        - ./karton.ini (subsystem local)
+
+        :param path: path to additional configuration file
+        """
+        if path is not None:
+            if not os.path.isfile(path):
+                raise IOError("Configuration file not found in " + path)
+            self.SEARCH_PATHS = self.SEARCH_PATHS + [path]
+
         self.config = configparser.ConfigParser()
-        self.config.read(path)
+        if not self.config.read(self.SEARCH_PATHS):
+            raise IOError("'karton.ini' configuration file not found")
 
         self.minio_config = dict(self.config.items("minio"))
         self.redis_config = dict(self.config.items("redis"))
