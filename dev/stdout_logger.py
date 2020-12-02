4fcc5687da8b27a7946cd365037305bc328a70b6
nazywam <nazywam@gmail.com>
2020-12-02 18:29:31 +0100

    Fix logger

--- dev/stdout_logger.py
+++ dev/stdout_logger.py
@@ -5,8 +5,9 @@ from karton.core.karton import LogConsumer
 class StdoutLogger(LogConsumer):
     identity = "karton.stdout-logger"
 
-    def process_log(self, event: dict):
-        print(event["message"], file=sys.stderr)
+    def process_log(self, event):
+        if event.get("type") == "log":
+            print(f"{event['name']}: {event['message']}", file=sys.stderr)
 
 
 if __name__ == "__main__":
