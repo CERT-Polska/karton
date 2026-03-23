70eafdfb64279f857415612a536e44b4ff503173
Maciej Kotowicz <mkotowicz@zscaler.com>
2026-03-23 18:02:19 +0100

    default to AWS_ env if s3 is not specified in config

--- karton/core/backend.py
+++ karton/core/backend.py
@@ -1,3 +1,4 @@
+import os
 import dataclasses
 import enum
 import json
@@ -288,9 +289,9 @@ class KartonBackend(KartonBackendBase):
             config, identity=identity, service_info=service_info
         )
 
-        endpoint = config.get("s3", "address")
-        access_key = config.get("s3", "access_key")
-        secret_key = config.get("s3", "secret_key")
+        endpoint = config.get("s3", "address") or os.getenv("AWS_ENDPOINT")
+        access_key = config.get("s3", "access_key") or os.getenv("AWS_ACCESS_ID_KEY")
+        secret_key = config.get("s3", "secret_key") or os.getenv("AWS_SECRET_ACCESS_KEY")
         iam_auth = config.getboolean("s3", "iam_auth")
 
         if not endpoint:
